5f614c052b774f6b113b1e319196c43f214d2e73
Jan Barfuß <janbar2001@gmail.com>
2023-12-06 22:46:25 +0100

    Day 3 done

--- 03/solution.py
+++ 03/solution.py
@@ -1,4 +1,4 @@
-file = open("sample.txt", "r")
+file = open("input.txt", "r")
 lines = file.read()
 lines = lines.strip()
 lines = lines.split("\n")
@@ -31,82 +31,38 @@ for i in range(len(lines)):
                 start = None
                 end = None
 print(score)
-
 score = 0
+numbers = [[[0 for k in range(1)] for j in range(len(lines))] for i in range(len(lines[0]))]
+print(numbers)
 for i in range(len(lines)):
+    start = None
+    end = None
     for j in range(len(lines[i])):
-        numbers = 0
-        star_score = 1
-        if lines[i][j] == "*":
-            if lines[i-1][j-1].isnumeric() and lines[i-1][j+1].isnumeric():
-                if lines[i-1][j].isnumeric():
-                    star_score *= int(lines[i][j-1:j+1])
-                    numbers += 1
-                else:
-                    if lines[i-1][j-3].isnumeric():
-                        star_score *= int(lines[i-1][j-3:j-1])
-                    else:
-                        star_score *= int(lines[i-1][j-2:j-1])
-                    if lines[i-1][j+3].isnumeric():
-                        star_score *= int(lines[i-1][j+1:j+3])
-                    else:
-                        star_score *= int(lines[i-1][j+1:j+2])
-                    numbers += 2
-            elif lines[i-1][j].isnumeric():
-                if lines[i-1][j-1].isnumeric():
-                    if lines[i][j-2].isnumeric():
-                        star_score *= int(lines[i][j-2:j])
-                    else:
-                        star_score *= int(lines[i][j-1:j])
-                else:
-                    if lines[i][j-2].isnumeric():
-                        star_score *= int(lines[i][j-2:j])
-                numbers += 1
-            elif lines[i-1][j-1].isnumeric():
-                if lines[i-1][j-3].isnumeric():
-                    star_score *= int(lines[i-1][j-3:j-1])
-                else:
-                    star_score *= int(lines[i-1][j-2:j-1])
-                numbers += 1
-            elif lines[i-1][j+1].isnumeric():
-                if lines[i-1][j+3].isnumeric():
-                    star_score *= int(lines[i-1][j+1:j+3])
-                else:
-                    star_score *= int(lines[i-1][j+1:j+2])
-                numbers += 1
-            if lines[i][j-1].isnumeric():
-                if lines[i][j-3].isnumeric():
-                    star_score *= int(lines[i][j-3:j-1])
-                else:
-                    star_score *= int(lines[i][j-2:j-1])
-                numbers += 1
-            if lines[i][j+1].isnumeric():
-                if lines[i][j+3].isnumeric():
-                    star_score *= int(lines[i][j+1:j+3])
-                else:
-                    star_score *= int(lines[i][j+1:j+2])
-                numbers += 1
-            if lines[i+1][j-1].isnumeric() and lines[i+1][j+1].isnumeric():
-                if lines[i+1][j].isnumeric():
-                    star_score *= int(lines[i][j-1:j+1])
-                    numbers += 1
-                else:
-                    if lines[i+1][j-3].isnumeric():
-                        star_score *= int(lines[i+1][j-3:j-1])
-                    else:
-                        star_score *= int(lines[i+1][j-2:j-1])
-                    if lines[i+1][j+3].isnumeric():
-                        star_score *= int(lines[i+1][j+1:j+3])
-                    else:
-                        star_score *= int(lines[i+1][j+1:j+2])
-                    numbers += 2
-                
-
-        if numbers == 2:
-            print(star_score)
-            score += star_score
-        numbers = 0
-        star_score = 1
-
-
+        if lines[i][j].isnumeric():
+            if start == None:
+                start = j
+                end = j
+            else:
+                end = j
+        if not lines[i][j].isnumeric() or j == len(lines[i])-1:
+            if start != None:
+                serial = False
+                for z in range(start-1,end+2):
+                    if z <0 or z >= len(lines[i]):
+                        continue
+                    if lines[i][z] == "*":
+                        numbers[i][z].append(int(lines[i][start:end+1])) 
+                    if i > 0 and lines[i-1][z] == "*":
+                        numbers[i-1][z].append(int(lines[i][start:end+1]))
+                    if i < len(lines)-1 and lines[i+1][z] == "*":
+                        numbers[i+1][z].append(int(lines[i][start:end+1]))
+                serial = False
+                start = None
+                end = None
+print(numbers)
+for i in range(len(numbers)):
+    for j in range(len(numbers[i])):
+        if len(numbers[i][j]) == 3:
+            score += numbers[i][j][1] * numbers[i][j][2]
+print(score)
 
